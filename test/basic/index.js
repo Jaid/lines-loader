8b8509109f76ad4de61b4dac1f6b0f063ccae56d
Jaid <jaid.jsx@gmail.com>
2020-01-11 13:07:58 +0100

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/basic/index.js
+++ test/basic/index.js
@@ -1,7 +1,7 @@
 import a from "./a.lines"
 import bUnsorted from "./b.lines"
 import bSorted from "./b.lines?sort"
-import rSorted from "./r.lines?random"
+import rSorted from "./r.lines?randomPick"
 
 console.log(a)
 console.log(bUnsorted)
